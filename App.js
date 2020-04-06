23c8fd2c0f59b768a9eaf9ba590bcee220579390
William A Rodriguez <wrod@mit.edu>
2020-04-06 00:56:34 -0400

    Added base of time and driving day blocks

--- App.js
+++ App.js
@@ -31,7 +31,12 @@ export default class App extends React.Component{
   render(){
     return (
       <View style={{...StyleSheet.absoluteFillObject,display:'flex',flexDirection: 'column', alignItems:'center',justifyContent:'center'}}>
-        <View style={{display:'flex',flexDirection:'row',top:50}}>
+        <View style={{display:'flex',flexDirection:'row',width:300,height:70,backgroundColor:'purple',borderRadius:15,alignItems:'center',justifyContent:'center'}}>
+          <Text style={{fontSize:50}}>9:41</Text>
+        </View>
+
+
+        <View style={{display:'flex',flexDirection:'row'}}>
           <View style={{width: BLOCK_WIDTH, height: BLOCK_HEIGHT, borderTopLeftRadius:15, backgroundColor: 'powderblue',alignItems:'center',justifyContent:'center'}}>
             <Text>Deceased</Text>
           </View>
@@ -42,7 +47,7 @@ export default class App extends React.Component{
             <Text>Recovered</Text>
           </View>
         </View>
-        <View style={{display:'flex',flexDirection:'row',top:50}}>
+        <View style={{display:'flex',flexDirection:'row'}}>
           <View style={{width: BLOCK_WIDTH, height: BLOCK_HEIGHT, borderBottomLeftRadius:15, backgroundColor: 'red',alignItems:'center',justifyContent:'center'}}>
             <Text>0</Text>
           </View>
@@ -53,6 +58,14 @@ export default class App extends React.Component{
             <Text>0</Text>
           </View>
         </View>
+        <View style={{display:'flex',flexDirection:'row'}}>
+          <View style={{backgroundColor:'blue',height:100,width:300,borderRadius:15,alignItems:'center',justifyContent:'center'}}>
+            <Text>Hoy es miércoles, día impar</Text>
+            <Text>No puedes guiar</Text>
+
+          </View>
+        </View>
+
       </View>
       )
   }
