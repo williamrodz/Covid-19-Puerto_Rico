b74e76a6f1cc33de7a4cf0900d502c5690a4140d
William A Rodriguez <wrod@mit.edu>
2020-04-10 23:36:31 -0400

    Municipalities and scroll view added

--- HomeScreen.js
+++ HomeScreen.js
@@ -17,6 +17,8 @@ import {
   View,
   Text,
   Button,
+  SafeAreaView,
+  ScrollView
 } from 'react-native';
 
 
@@ -25,6 +27,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import RNFetchBlob from 'rn-fetch-blob'
 const DATA_LAG_DAYS = 1
+
+const SCROLLVIEW_MARGIN = 5
 
 const BLOCK_WIDTH = 100
 const BLOCK_HEIGHT = 60
@@ -71,23 +75,27 @@ function getTwoDigitNumber(number){
 //   </View>
 // </View>
 
-function getMunicipiosRowsWithData(municipios,limit=2){
+function getMunicipiosRowsWithData(municipios){
   const allContent = []
-  MUNICIPIO_BLOCK_WIDTH = 100
-  MUNICIPIO_BLOCK_HEIGHT = 100
+  MUNICIPIO_BLOCK_WIDTH = 125
+  MUNICIPIO_BLOCK_HEIGHT = 40
   const municipioNames = Object.keys(municipios)
-
   for (var i = 0; i < municipioNames.length; i++) {
     const municipio = municipioNames[i]
-    if (i == limit){
-      break
-    }
+    const borderTopLeftRadius = i == 0 ? 15 : 0
+    const borderTopRightRadius = i == 0 ? 15 : 0
+    const borderBottomLeftRadius = i == municipioNames.length - 1 ? 15 : 0
+    const borderBottomRightRadius = i == municipioNames.length - 1 ? 15 : 0
+    // const cellColor
+    // // if municipio.totalCases >= 30
+
+
     var rowContent = (
       <View key={municipio} style={{display:'flex',flexDirection:'row'}}>
-        <View style={{width: MUNICIPIO_BLOCK_WIDTH, height: MUNICIPIO_BLOCK_HEIGHT, backgroundColor: 'chartreuse',alignItems:'center',justifyContent:'center'}}>
+        <View style={{borderColor: 'black', borderWidth: 1, borderTopLeftRadius: borderTopLeftRadius,borderBottomLeftRadius: borderBottomLeftRadius, width: MUNICIPIO_BLOCK_WIDTH, height: MUNICIPIO_BLOCK_HEIGHT, backgroundColor: 'ghostwhite',alignItems:'center',justifyContent:'center'}}>
           <Text>{municipio}</Text>
         </View>
-        <View style={{width: MUNICIPIO_BLOCK_WIDTH, height: MUNICIPIO_BLOCK_HEIGHT, backgroundColor: 'green',alignItems:'center',justifyContent:'center'}}>
+        <View style={{borderColor: 'black', borderWidth: 1,borderTopRightRadius: borderTopRightRadius,borderBottomRightRadius:borderBottomRightRadius, width: MUNICIPIO_BLOCK_WIDTH, height: MUNICIPIO_BLOCK_HEIGHT, backgroundColor: 'ghostwhite',alignItems:'center',justifyContent:'center'}}>
           <Text>{municipios[municipio].totalCases}</Text>
         </View>
       </View>
@@ -241,95 +249,92 @@ export default class Home extends React.Component{
     const drivingDayDescription = `Hoy es ${DAYS_OF_WEEK_SPANISH[dayOfWeek]}, día para tabillas ${dayOfWeek % 2 != 0 ? "impares": "pares"}`
 
     return (
-      <View style={{...StyleSheet.absoluteFillObject,display:'flex',flexDirection: 'column', alignItems:'center',justifyContent:'center'}}>
-        <View style={{display:'flex',flexDirection:'row',width:300,height:70,backgroundColor:'purple',borderRadius:15,alignItems:'center',justifyContent:'center'}}>
-          <Text style={{fontSize:50}}>9:41</Text>
-        </View>
+      <SafeAreaView style={{...StyleSheet.absoluteFillObject,display:'flex',flexDirection: 'column', alignItems:'center',justifyContent:'center'}}>
+        <ScrollView contentContainerStyle={{flexGrow:1,display:'flex',flexDirection:'column',alignItems: 'center',margin:SCROLLVIEW_MARGIN}}>
+          <View style={{display:'flex',flexDirection:'row',width:300,height:70,backgroundColor:'purple',borderRadius:15,alignItems:'center',justifyContent:'center'}}>
+            <Text style={{fontSize:50}}>9:41</Text>
+          </View>
 
-
-        <View style={{display:'flex',flexDirection:'row'}}>
-          <View style={{width: BLOCK_WIDTH, height: BLOCK_HEIGHT, borderTopLeftRadius:15, backgroundColor: 'powderblue',alignItems:'center',justifyContent:'center'}}>
-            <Text>Deceased</Text>
+          <View style={{display:'flex',flexDirection:'row'}}>
+            <View style={{width: BLOCK_WIDTH, height: BLOCK_HEIGHT, borderTopLeftRadius:15, backgroundColor: 'powderblue',alignItems:'center',justifyContent:'center'}}>
+              <Text>Deceased</Text>
+            </View>
+            <View style={{width: BLOCK_WIDTH, height: BLOCK_HEIGHT, backgroundColor: 'skyblue',alignItems:'center',justifyContent:'center'}}>
+              <Text>Casos Positivos</Text>
+            </View>
+            <View style={{width: BLOCK_WIDTH, height: BLOCK_HEIGHT,borderTopRightRadius:15,  backgroundColor: 'steelblue',alignItems:'center',justifyContent:'center'}}>
+              <Text>Recovered</Text>
+            </View>
           </View>
-          <View style={{width: BLOCK_WIDTH, height: BLOCK_HEIGHT, backgroundColor: 'skyblue',alignItems:'center',justifyContent:'center'}}>
-            <Text>Casos Positivos</Text>
+          <View style={{display:'flex',flexDirection:'row'}}>
+            <View style={{width: BLOCK_WIDTH, height: BLOCK_HEIGHT, borderBottomLeftRadius:15, backgroundColor: 'red',alignItems:'center',justifyContent:'center'}}>
+              <Text>{this.state.deceased}</Text>
+            </View>
+            <View style={{width: BLOCK_WIDTH, height: BLOCK_HEIGHT, backgroundColor: 'green',alignItems:'center',justifyContent:'center'}}>
+              <Text>{this.state.positiveToday}</Text>
+            </View>
+            <View style={{width: BLOCK_WIDTH, height: BLOCK_HEIGHT, borderBottomRightRadius:15, backgroundColor: 'blue',alignItems:'center',justifyContent:'center'}}>
+              <Text>{this.state.recovered}</Text>
+            </View>
           </View>
-          <View style={{width: BLOCK_WIDTH, height: BLOCK_HEIGHT,borderTopRightRadius:15,  backgroundColor: 'steelblue',alignItems:'center',justifyContent:'center'}}>
-            <Text>Recovered</Text>
+          <View style={{display:'flex',flexDirection:'row'}}>
+            <View style={{backgroundColor:canDriveToday ? GO_COLOR : STOP_COLOR,height:100,width:300,borderRadius:15,alignItems:'center',justifyContent:'center'}}>
+              <Text>{drivingDayDescription}</Text>
+              <Text>{canDriveToday ? CAN_DRIVE_TEXT : CANT_DRIVE_TEXT}</Text>
+            </View>
           </View>
-        </View>
-        <View style={{display:'flex',flexDirection:'row'}}>
-          <View style={{width: BLOCK_WIDTH, height: BLOCK_HEIGHT, borderBottomLeftRadius:15, backgroundColor: 'red',alignItems:'center',justifyContent:'center'}}>
-            <Text>{this.state.deceased}</Text>
-          </View>
-          <View style={{width: BLOCK_WIDTH, height: BLOCK_HEIGHT, backgroundColor: 'green',alignItems:'center',justifyContent:'center'}}>
-            <Text>{this.state.positiveToday}</Text>
-          </View>
-          <View style={{width: BLOCK_WIDTH, height: BLOCK_HEIGHT, borderBottomRightRadius:15, backgroundColor: 'blue',alignItems:'center',justifyContent:'center'}}>
-            <Text>{this.state.recovered}</Text>
-          </View>
-        </View>
-        <View style={{display:'flex',flexDirection:'row'}}>
-          <View style={{backgroundColor:canDriveToday ? GO_COLOR : STOP_COLOR,height:100,width:300,borderRadius:15,alignItems:'center',justifyContent:'center'}}>
-            <Text>{drivingDayDescription}</Text>
-            <Text>{canDriveToday ? CAN_DRIVE_TEXT : CANT_DRIVE_TEXT}</Text>
-          </View>
-        </View>
-        <View style={{display:'flex',flexDirection:'row'}}>
-          <LineChart
-              data={{
-                labels: this.state.dateAbbreviations ? this.state.dateAbbreviations : ["January", "February", "March", "April", "May", "June"],
-                datasets: [
-                  {
-                    data: this.state.weeksPositives ? this.state.weeksPositives : [
-                      Math.random() * 51,
-                      Math.random() * 100,
-                      Math.random() * 100,
-                      Math.random() * 100,
-                      Math.random() * 100,
-                      Math.random() * 100
-                    ]
+          <View style={{display:'flex',flexDirection:'row'}}>
+            <LineChart
+                data={{
+                  labels: this.state.dateAbbreviations ? this.state.dateAbbreviations : ["January", "February", "March", "April", "May", "June"],
+                  datasets: [
+                    {
+                      data: this.state.weeksPositives ? this.state.weeksPositives : [
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100,
+                      ]
+                    }
+                  ]
+                }}
+                width={Dimensions.get("window").width - SCROLLVIEW_MARGIN * 2} // from react-native
+                height={220}
+                yAxisLabel=""
+                yAxisSuffix=""
+                yAxisInterval={1} // optional, defaults to 1
+                chartConfig={{
+                  backgroundColor: "#e26a00",
+                  backgroundGradientFrom: "#3498db",
+                  backgroundGradientTo: "#34c5db",
+                  decimalPlaces: 2, // optional, defaults to 2dp
+                  color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                  labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                  style: {
+                    borderRadius: 16
+                  },
+                  propsForDots: {
+                    r: "6",
+                    strokeWidth: "2",
+                    stroke: "#ffa726"
                   }
-                ]
-              }}
-              width={Dimensions.get("window").width} // from react-native
-              height={220}
-              yAxisLabel=""
-              yAxisSuffix=""
-              yAxisInterval={1} // optional, defaults to 1
-              chartConfig={{
-                backgroundColor: "#e26a00",
-                backgroundGradientFrom: "#3498db",
-                backgroundGradientTo: "#34c5db",
-                decimalPlaces: 2, // optional, defaults to 2dp
-                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                style: {
+                }}
+                bezier
+                style={{
+                  marginVertical: 8,
                   borderRadius: 16
-                },
-                propsForDots: {
-                  r: "6",
-                  strokeWidth: "2",
-                  stroke: "#ffa726"
-                }
-              }}
-              bezier
-              style={{
-                marginVertical: 8,
-                borderRadius: 16
-              }}
-            />
-        </View>
-        {this.state.municipioDataToday ? getMunicipiosRowsWithData(this.state.municipioDataToday) : <Text>Hi</Text>}
+                }}
+              />
+          </View>
+          {this.state.municipioDataToday ? getMunicipiosRowsWithData(this.state.municipioDataToday) : <Text>Hi</Text>}
 
-        <View style={{display:'flex',flexDirection:'row'}}>
-          <Button title="Settings" onPress={() => this.settingsButton()}/>
+          <View style={{display:'flex',flexDirection:'row'}}>
+            <Button title="Settings" onPress={() => this.settingsButton()}/>
 
-        </View>
+          </View>
 
 
-
-      </View>
+        </ScrollView>
+      </SafeAreaView>
       )
   }
 }
